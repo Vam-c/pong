d2a511d81fb1768aa5ae940d02220f9c1adc8400
Vam-c <vamsee.somasi@gmail.com>
2025-08-28 23:33:52 +0530

    Finish game

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const board = document.getElementById("boundary");
+const boardBounds = board.getBoundingClientRect();
+const borderWidth = parseInt(getComputedStyle(board).borderWidth);
 const paddleHeight = document.getElementById("left-paddle").clientHeight;
-const boardHeight = document.getElementById("boundary").clientHeight;
-const paddleSpeed = boardHeight / paddleHeight;
+const paddleSpeed = board.clientHeight / paddleHeight;
+const ballWidth = document.getElementById("ball").clientWidth;
+const baseSpeed = 2;
 
 // We use to separate inputs to allow simultaneous keydowns and ups
 // without overriding with 0;
@@ -8,11 +12,19 @@ let leftInput = [0, 0];
 let rightInput = [0, 0];
 let posLeftPaddle = 0;
 let posRightPaddle = 0;
+let score = { left: 0, right: 0 };
 
-// TODO initiate ball with constant speed in y and 
-// random in x within a range.
-let ball = { speedX: 1, speedY: 1, x: boardHeight / 2, y: window.innerWidth / 2 };
-console.log(ball)
+const getRandomSpeedY = () => {
+    let num = 0;
+    while (num === 0)
+        num = Math.floor(Math.random() * 2 - 1);
+
+    return num * baseSpeed;
+};
+
+// initiate ball with constant speed in x and 
+// random y within a range.
+let ball = { speedX: baseSpeed, speedY: getRandomSpeedY(), y: (boardBounds.bottom + boardBounds.top) / 2, x: (window.innerWidth - ballWidth) / 2 };
 
 // store key presses in input arrays.
 document.addEventListener("keydown", (e) => {
@@ -38,12 +50,12 @@ const render = () => {
     rightPaddle.style.top = posRightPaddle + "px";
 
     const ballDiv = document.getElementById("ball");
-    ballDiv.style.top = ball.x + "px";
-    ballDiv.style.left = ball.y + "px";
+    ballDiv.style.top = ball.y + "px";
+    ballDiv.style.left = ball.x + "px";
 };
 
 const updatePaddles = () => {
-    const maxTop = boardHeight - paddleHeight;
+    const maxTop = board.clientHeight - paddleHeight;
     let updatedLeftTop = posLeftPaddle + (-leftInput[0] + leftInput[1]) * paddleSpeed;
     if (updatedLeftTop < 0) updatedLeftTop = 0;
     if (updatedLeftTop > maxTop) updatedLeftTop = maxTop;
@@ -55,13 +67,56 @@ const updatePaddles = () => {
     posRightPaddle = updatedRightTop
 };
 
+const collideLeft = () => {
+    const leftPaddle = document.getElementById("left-paddle").getBoundingClientRect();
+
+    if (ball.x <= boardBounds.left + borderWidth) return true;
+
+    if (
+        ball.x <= leftPaddle.right
+        && ball.x >= leftPaddle.right - 3
+        && ball.y + ballWidth > leftPaddle.top
+        && ball.y < leftPaddle.bottom
+    ) ball.speedX = Math.abs(ball.speedX);
+
+    return false;
+};
+
+const collideRight = () => {
+    const rightPaddle = document.getElementById("right-paddle").getBoundingClientRect();
+
+    if (ball.x + ballWidth >= boardBounds.right - borderWidth) return true;
+
+    if (
+        ball.x + ballWidth >= rightPaddle.left
+        && ball.x + ballWidth <= rightPaddle.left + 3
+        && ball.y + ballWidth > rightPaddle.top
+        && ball.y < rightPaddle.bottom
+    ) ball.speedX = -Math.abs(ball.speedX);
+
+    return false;
+};
+
 const updateBall = () => {
     ball.x += ball.speedX;
     ball.y += ball.speedY;
 
-    // TODO ball should bounce on border walls and paddles.
+    // vertical collision.
+    const maxTop = boardBounds.top + borderWidth;
+    const maxBottom = boardBounds.bottom - borderWidth - ballWidth;
+    if (ball.y <= maxTop || ball.y >= maxBottom) ball.speedY = -ball.speedY;
 
-    // TODO update score if ball touches vertical walls.
+    // horizontal collision.
+    if (collideLeft()) {
+        score.right += 1;
+        document.getElementById("score-right").innerText = score.right;
+        ball = { speedX: baseSpeed, speedY: getRandomSpeedY(), y: (boardBounds.bottom + boardBounds.top) / 2, x: (window.innerWidth - ballWidth) / 2 };
+    } else if (collideRight()) {
+        score.left += 1;
+        document.getElementById("score-left").innerText = score.left;
+        ball = { speedX: -baseSpeed, speedY: getRandomSpeedY(), y: (boardBounds.bottom + boardBounds.top) / 2, x: (window.innerWidth - ballWidth) / 2 };
+    }
+
 
     // TODO reset game if score >9.
 
